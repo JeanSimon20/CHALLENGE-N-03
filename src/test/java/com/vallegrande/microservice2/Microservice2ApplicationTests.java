863dc295ce96a6fd81393ef2fb550353d34d6678
JeanSimon20 <jean.simon@vallegrande.edu.pe>
2022-07-17 15:46:13 -0500

    Dockerfile

--- src/test/java/com/vallegrande/microservice2/Microservice2ApplicationTests.java
+++ src/test/java/com/vallegrande/microservice2/Microservice2ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.vallegrande.microservice1;
+package com.vallegrande.microservice2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Microservice1ApplicationTests {
+class Microservice2ApplicationTests {
 
     @Test
     void contextLoads() {
